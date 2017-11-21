48ce2d8086e7136775f9876d5dbbc2b956075e4e
Ben Rose <benjaminshannonrose@gmail.com>
2017-11-21 13:09:23 -0500

    update

--- features/pages/BookingsPage.js
+++ features/pages/BookingsPage.js
@@ -6,6 +6,7 @@ import { acceptAlert} from "../support/helper/alertHelper";
 class BookingsPage extends Page {
 
     get hotelName() { return $$('.hotelName');}
+    get secondHotelName() { return $('body > div.container-fluid > div > div:nth-child(2) > p.col-xs-10.hotelName');}
     get hotelView() { return $('body > div.container-fluid > div > div:nth-child(1) > button.col-xs-1.btn.btn-primary.btn-sm.view');}
     get streetAddress() { return browser.elements('.street-address');}
     get mapCloser() { return $('#modalBody > div > div > div:nth-child(1) > div:nth-child(4) > div:nth-child(4) > div > img');}
@@ -18,6 +19,7 @@ class BookingsPage extends Page {
     }
 
     clicksViewBookingButton() {
+        this.secondHotelName.click();
         this.hotelView.click();
     }
 
